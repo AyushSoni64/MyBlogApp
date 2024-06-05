7bdf633acdd65d5aa3caa0fc1b7ad120ee288528
Ayush Soni <ayush.soni@successive.tech>
2024-06-05 18:36:57 +0530

    Update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,11 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.get("health-check", (req, res)=>{
+    res.send("Everyting is working")
+})
+
 app.use(router);
 app.use(notFound);
 app.use(errorHandler);
