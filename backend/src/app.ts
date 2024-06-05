f83a6f38590fb81da55504ec5b66934c4883a6c9
Ayush Soni <ayush.soni@successive.tech>
2024-06-05 18:41:29 +0530

    Update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.get("health-check", (req, res)=>{
+app.get("/health-check", (req, res)=>{
     res.send("Everyting is working")
 })
 
